6cebd6a54ead3b9230d2ca39154a422e05043a2f
batuhansicakyuz <batuhansicakyuz@gmail.com>
2023-06-20 23:45:55 +0300

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { UeberunsComponent } from './ueberuns/ueberuns.component';
 import { HomeComponent } from './home/home.component';
+import { SecondSection } from './secondsection/secondsection.component';
 
 const routes: Routes = [
-{path:'',component:FirstComponent},
-{ path: 'first-component', component: FirstComponent },
+{path:'',component:FirstComponent },
   {path:'home-component',component:HomeComponent},
+  { path: 'first-component', component: FirstComponent },
   { path: 'ueberuns-component', component: UeberunsComponent },
-  { path: '', component: UeberunsComponent },
+  { path: 'secondsection-component', component: SecondSection },
 //   { path:'home', component: HomeComponent},
 ];
 
--- src/app/secondsection/secondsection.component.ts
+++ src/app/secondsection/secondsection.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./secondsection.component.css']
 })
 export class SecondSection {
+mapOptions: google.maps.MapOptions = {
+    center: { lat: 52.52346922653795, lng: 13.413399073765264 },
+    zoom: 12,
+    zoomControl: false,
+       mapTypeControl: false,
+       streetViewControl: false,
+       fullscreenControl: false
+  };
 
+panelOpenState = false;
+
+showFiller = false;
 }

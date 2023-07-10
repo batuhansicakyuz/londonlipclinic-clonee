d2e36eb1a29f13bc87b1eaa73379ffaaf22e5b04
batuhansicakyuz <batuhansicakyuz@gmail.com>
2023-07-10 11:56:58 +0200

    developed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+constructor(){}
+ngOnInit(){
+AOS.init();
+}
   title = 'ng_clone_online_reader';
 }
